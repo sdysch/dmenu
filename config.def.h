5d556acbe62db6c98739c65f824889ef7b09fb54
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-21 13:53:26 +0100

    FOrmatting

--- config.def.h
+++ config.def.h
@@ -8,12 +8,12 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	/*							fg         bg       */
+	[SchemeNorm]		  = { "#bbbbbb", "#222222" },
+	[SchemeSel]  		  = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut]			  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
