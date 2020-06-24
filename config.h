75a20262e2b73eac90153942e05cdfc30352d41c
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-24 10:45:28 +0100

    Update defaults

--- config.h
+++ config.h
@@ -4,16 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"xos4 Terminus:size=12"
+	//"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*							fg         bg       */
+	[SchemeNorm]		  = { "#bbbbbb", "#222222" },
+	[SchemeSel]  		  = { "#434C5E", "#A3BE8C" },
+	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#D8DEE9", "#2E3440" },
+	[SchemeOut]			  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
